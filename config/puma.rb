91cee271dab4bfc6650fa871ab02c2225b6d36d7
whoiswentz <wentz.vinicios@gmail.com>
2024-05-09 14:51:34 -0300

    Set up Render deployment

--- config/puma.rb
+++ config/puma.rb
@@ -16,6 +16,12 @@ if ENV["RAILS_ENV"] == "production"
   require "concurrent-ruby"
   worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
   workers worker_count if worker_count > 1
+
+  # Use the `preload_app!` method when specifying a `workers` number.
+  # This directive tells Puma to first boot the application and load code
+  # before forking the application. This takes advantage of Copy On  Write
+  # process behavior so workers use less memory.
+  preload_app!
 end
 
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
